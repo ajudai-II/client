5a6743f56ebd11a414860031bfd6504bf48f2f39
raulhenrique-x3 <raulheri43@hotmail.com>
2023-11-05 13:09:45 -0300

    fix: screens

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { Input } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { HTMLInputTypeAttribute } from "react";
 import styles from "./input.module.scss";
 import { UseFormRegister } from "react-hook-form";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
-interface IInput {
+interface IInput extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   type: HTMLInputTypeAttribute;
   name: string;
@@ -21,15 +22,27 @@ const CustomInput: React.FC<IInput> = ({
   placeholder,
   errors,
 }) => {
+  const [isPassword, setIsPassword] = useState(false);
   return (
     <div>
       {label && <label className={styles.inputLabel}>{label}</label>}
-      <Input
-        borderColor={errors?.[name] ? "red" : undefined}
-        type={type}
-        placeholder={placeholder}
-        {...register(name, { required: true })}
-      />
+      <InputGroup>
+        <Input
+          borderColor={errors?.[name] ? "red" : undefined}
+          type={isPassword ? type : "text"}
+          placeholder={placeholder}
+          {...register(name, { required: true })}
+        />
+        {type === "password" && (
+          <InputRightElement onClick={() => setIsPassword(!isPassword)}>
+            {isPassword ? (
+              <ViewOffIcon color="gray.500" />
+            ) : (
+              <ViewIcon color="gray.500" />
+            )}
+          </InputRightElement>
+        )}
+      </InputGroup>
       {errors[name] && (
         <p className={styles.errorMessage}>{errors[name].message}</p>
       )}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Box, Button, Container } from "@chakra-ui/react";
+import { Box, Button, Container, Text, useToast } from "@chakra-ui/react";
 import CustomInput from "@/components/Input/Input";
 import Image from "next/image";
 import schema from "@/utils/schema/Login";
 import { api } from "@/services/api";
+import { AxiosError } from "axios";
 
 const Login = () => {
   const {
@@ -14,19 +15,26 @@ const Login = () => {
     watch,
     formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
+  const toast = useToast();
 
   const handleLogin = async () => {
     const formData = watch();
     try {
       await api.post("/login", {
         ...formData,
-        createdAt: new Date(),
       });
     } catch (error) {
-      console.error("Erro na solicitação:", error);
+      const errorMessage =
+        (error as AxiosError<{ message: string }>).response?.data?.message ||
+        "Erro desconhecido";
+      toast({
+        title: errorMessage,
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
     }
   };
-
   return (
     <Box
       display={"flex"}
@@ -37,12 +45,19 @@ const Login = () => {
     >
       <Container>
         <form onSubmit={onSubmit(handleLogin)}>
-          <Image
-            src={"/icons/logo.svg"}
-            alt={"logo do ajudai"}
-            width={192}
-            height={192}
-          />
+          <Box
+            w={"100%"}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Image
+              src={"/icons/logo.svg"}
+              alt={"logo do ajudai"}
+              width={192}
+              height={192}
+            />
+          </Box>
 
           <CustomInput
             label="Email"
@@ -54,7 +69,7 @@ const Login = () => {
 
           <CustomInput
             label="Senha"
-            type="password"
+            type="text"
             register={register}
             name="password"
             placeholder="Digite sua senha"
