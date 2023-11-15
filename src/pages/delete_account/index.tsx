439c41b7ff2ea863aa5510c97df6cd609fd743d1
Thiago <1thiago1henrique@gmail.com>
2023-11-15 15:34:30 -0300

    fix: delete screen update

--- src/pages/delete_account/index.tsx
+++ src/pages/delete_account/index.tsx
@@ -1,31 +1,43 @@
-import { Box, Container, FormControl, FormLabel, Image, Input, Heading, Button, Text, FormHelperText } from "@chakra-ui/react";
+import Header from "@/components/Header/Header";
+import { Box, Container, Image, Heading, Button, Text } from "@chakra-ui/react";
+import Axios from "axios"; // Import Axios for making HTTP requests
 import React from "react";
 
 const delete_account = () => {
+  const handleDeleteAccount = async () => {
+    try {
+      // Replace 'YOUR_SERVER_URL' with the actual URL where your server is hosted
+      const response = await Axios.delete('YOUR_SERVER_URL/api/users/:userId'); // Adjust the URL as needed
+      console.log(response.data.message); // Log the server response
+      // Optionally, you can navigate to another page or show a success message
+    } catch (error) {
+      console.error(error);
+      // Handle errors, show an error message, etc.
+    }
+  };
+
   return (
-    <Container display={"flex"} justifyContent={"center"} alignItems={"center"} w={"100%"} h={"100dvh"}>
-      <Box display={"flex"} justifyContent={"center"} alignItems={"center"} flexDirection={"column"} minW={"100%"} height={"100%"}>
-        <Image w={"80px"} h={"80px"} src="/icons/sad-icon.svg" objectFit={"cover"} alt="Profile user" borderRadius={"50%"} mt={"2.375rem"} alignSelf={"center"} marginBottom={"2rem"} />
+    <>
+      <Header />
+      <Container display={"flex"} justifyContent={"center"} alignItems={"center"} w={"100%"} h={"100dvh"}>
+        <Box display={"flex"} justifyContent={"center"} alignItems={"center"} flexDirection={"column"} minW={"100%"} height={"100%"}>
+          <Image w={"80px"} h={"80px"} src="/icons/sad-icon.svg" objectFit={"cover"} alt="Profile user" borderRadius={"50%"} mt={"2.375rem"} alignSelf={"center"} marginBottom={"2rem"} />
 
-        <Box w={"100%"}>
-          <Heading color={"#16161b"}>Triste ter que ser assim</Heading>
-          <Text paddingBottom={"2rem"}>Esperamos ver você por aqui novamente!</Text>
-        </Box>
-        <FormControl>
-          <FormLabel fontWeight={"700"} color={"#16161b"}>
-            Insira seu email
-          </FormLabel>
-          <Input type="email" minW={"320px"} color={"#16161b"} />
-          <FormHelperText color={"#16161b"}>Tenha em mente que essa opção não poderá ser desfeita.</FormHelperText>
-        </FormControl>
+          <Box w={"100%"} display={"flex"} flexDirection={"column"} alignContent={"center"}>
+            <Heading color={"#16161b"} textAlign={"center"}>
+              Triste ter que ser assim
+            </Heading>
+            <Text textAlign={"center"}>Esperamos ver você por aqui novamente!</Text>
+          </Box>
 
-        <Box>
-          <Button w={"100%"} marginTop={16} color={"#fff"} background={"#fa8fb1"} size="md" type="submit">
-            Deletar minha conta
-          </Button>
+          <Box display={"flex"} p={"2rem"}>
+            <Button w={"100%"} color={"#fff"} background={"#fa8fb1"} size="md" onClick={handleDeleteAccount}>
+              Deletar minha conta
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </>
   );
 };
 
