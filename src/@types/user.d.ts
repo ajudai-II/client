b2134d1748856687f56a90b859a43fcaf2b6710c
Ana St <anna1100st@gmail.com>
2023-12-08 01:16:12 -0300

    fix: account-screen

--- src/@types/user.d.ts
+++ src/@types/user.d.ts
@@ -1,9 +1,11 @@
 export interface IUser {
+  address: any;
   _id: string;
   name: string;
   email: string;
   phone: string;
   cpf: string;
   password: string;
+  photo: string;
   createdAt: Date;
 }
